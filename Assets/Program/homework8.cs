7bd3d3ebd24d89d793e4255e339a0276b5af1847
Abdulhamead22 <abdalmashharami@gmail.com>
2024-10-29 13:18:50 +0200

    new update

--- Assets/Program/homework8.cs
+++ Assets/Program/homework8.cs
@@ -22,7 +22,16 @@ while (true)
     }
     Debug.Log(number);
 }
-
+int counter=0;
+string sentence="";
+string[] word={"Cat", "Dog","Car", "Pizza", "Hat", "Fish", "Tree", "Monkey", "Ball", "Bird"};
+while (counter<7)
+{
+    int rangeOfWords=Random.Range(0,word.Length);
+    sentence+=word[rangeOfWords];
+    counter++;
+}
+Debug.Log(sentence);
     }
 
     // Update is called once per frame
