a9389779c952341daf7e9ebe3d67cff0e155b560
Abdulhamead22 <abdalmashharami@gmail.com>
2024-11-13 12:48:29 +0200

    new

--- Assets/Program/GameManager.cs
+++ Assets/Program/GameManager.cs
@@ -24,10 +24,10 @@ public class GameManager : MonoBehaviour
         // Debug.Log("Name is "+ player.Name+" the Health"+player.Health);
         playercharactar player1=new playercharactar("Abd",75);
         player1.Heal(10);
-        Debug.Log("Name is "+ player1.Name+" the Health"+player1.Health);
+        Debug.Log("Name is "+ player1.Name+" the Health "+player1.Health);
         Enemy enemy = new Enemy("Malk", 80);
         enemy.Attack(enemy,10);
-        Debug.Log("Name is "+ enemy.Name+" the Health"+enemy.Health);
+        Debug.Log("Name is "+ enemy.Name+" the Health "+enemy.Health);
     }
 
     // Update is called once per frame
